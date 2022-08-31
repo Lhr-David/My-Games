eab0cfb37bb5db86f2c0d574b7433e645cbf6485
Aieylisyh <Aieylisyh@users.noreply.github.com>
2022-08-31 15:02:31 +0800

    wip

---
+++ TankRunner/Assets/_Completed-Assets/Scripts/com/Util/Ui/ResizeRectTransform.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using Text = TMPro.TextMeshProUGUI;
+
+namespace com
+{
+    public class ResizeRectTransform : MonoBehaviour
+    {
+        public RectTransform target;
+        public RectTransform reference;
+        public RectTransform reference2;
+        public Text referenceOverrideText;
+        public bool resizeHeight;
+        public bool resizeWidth;
+        public float heightOffset;
+        public float widthOffset;
+        public bool toResize = false;
+        private bool _toResize2 = false;
+
+        public void Update()
+        {
+            if (toResize)
+            {
+                toResize = false;
+                Resize();
+            }
+
+            if (_toResize2)
+            {
+                toResize = true;
+                _toResize2 = false;
+            }
+        }
+
+        public void ResizeLater()
+        {
+            toResize = true;
+            _toResize2 = true;
+        }
+
+        void Resize()
+        {
+            //Debug.Log("size");
+            var size = target.sizeDelta;
+            //Debug.Log(size);
+            var sizeRef = reference.sizeDelta;
+            //Debug.Log(sizeRef);
+            if (referenceOverrideText != null)
+            {
+                //refText.autoSizeTextContainer = true;
+                sizeRef.x = referenceOverrideText.renderedWidth;
+                sizeRef.y = referenceOverrideText.renderedHeight;
+                // Debug.Log(sizeRef);
+            }
+            if (reference2 != null)
+            {
+                sizeRef.x += reference2.sizeDelta.x;
+                sizeRef.y += reference2.sizeDelta.y;
+            }
+
+            if (resizeWidth)
+            {
+                sizeRef.x = Mathf.Max(0, sizeRef.x);
+                size.x = sizeRef.x + widthOffset;
+            }
+
+            if (resizeHeight)
+            {
+                sizeRef.y = Mathf.Max(0, sizeRef.y);
+                size.y = sizeRef.y + heightOffset;
+            }
+            //Debug.Log(size);
+            target.sizeDelta = size;
+        }
+    }
+}
